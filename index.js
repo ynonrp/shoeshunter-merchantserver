c233833e417da60c61457722bf5c568df4172112
ballighdev <balligh.dev@gmail.com>
2020-04-16 03:04:07 +0700

    make it better

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
 const app = express()
 const PORT = process.env.PORT || 4000
 app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
