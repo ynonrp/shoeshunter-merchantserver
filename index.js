822a66637f93573645d904d307d9ea9ae7e2802e
ballighdev <balligh.dev@gmail.com>
2020-04-16 03:16:52 +0700

    test

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const PORT = process.env.PORT || 4000
 app.use(logger('dev'))
 app.use(bodyParser.json())
 
-app.use('/' , indexRoutes)
-//app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
+app.use('/test' , indexRoutes)
+app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
