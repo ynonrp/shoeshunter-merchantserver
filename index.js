6f951e70d4072176de55f8a6fd6afa683c842752
ballighdev <balligh.dev@gmail.com>
2020-04-16 03:15:25 +0700

    test

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 
+const apiRoutes = require('./routes/apiRoutes')
+const indexRoutes = require('./routes/indexRoutes')
 const app = express()
 const PORT = process.env.PORT || 4000
-app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.use('/' , indexRoutes)
+//app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
