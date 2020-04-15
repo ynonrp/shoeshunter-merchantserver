5f154b937ab0b9fb7197c65f5c6121c2b91eb257
ballighdev <balligh.dev@gmail.com>
2020-04-16 03:20:00 +0700

    test

--- routes/indexRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res) {res.send("These aren't the Droids your looking for, move along")});
-
-router.get('/version', function(req, res) {res.send("1.1.2")});
-
-
-module.exports = router;
